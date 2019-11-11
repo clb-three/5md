9fff17e639ee099e964465a437ad192edb918143
Ben Steenhoek <benjis@iastate.edu>
2019-11-10 22:59:14 -0600

    Add comments

--- game.py
+++ game.py
@@ -3,6 +3,7 @@
 from input import get_input
 from enemy import Enemy
 
+# The enemy we're fighting
 enemy = Enemy()
 
 # Main game loop
@@ -14,14 +15,19 @@ while True:
     # Get input
     operation, args = get_input()
 
+    # Decide what operation to do
     if operation == 'play':
         # Attack with the card
         enemy.attack(args[0])
     elif operation == 'quit':
+        # Quit the game
         break
     elif operation == '<3':
+        # Love on u
         print('3<')
     else:
+        # Catch any command that we don't know
+        # and let the user know about it
         print('unrecognized command')
 
     # break out when the enemy is dead
