df7971e00b504cc02adecc45c441ab7534b14a4e
Ben Steenhoek <benjis@iastate.edu>
2019-11-10 22:48:17 -0600

    Split input into another file

--- input.py
+++ input.py
@@ -1,48 +1,4 @@
-# input.py: takes input and executes commands based on the input
-
-enemy = ['sword', 'shield', 'bow']
-
-def matches(card, enemy):
-    '''
-    Return true if card matches a card in enemy
-    Return false if none of the cards match
-    '''
-
-    match = None
-
-    # Take a field off the enemy
-    # When we play that card
-    for enemy_card in enemy:
-        if card == enemy_card:
-            match = enemy_card # marking card for removal
-
-    if match != None:
-        return True
-    else:
-        return False
-
-def print_enemy_health(enemy):
-    '''
-    Print out the enemy's cards and get the user's attack
-    '''
-
-    print('Enemy HP:')
-    # C++/Java/JS:
-    # for(int i = 0; i < enemy.size(); i ++)
-    # card = enemy[i]
-    for card in enemy:
-        print(card)
-
-def attack(card, enemy):    
-    '''
-    Test for a match and remove it from the
-    enemy's health if it's a match
-    '''
-
-    if matches(card, enemy):
-        enemy.remove(card)
-    else:
-        print('No match')
+# input.py: take input from the user
 
 def get_input():
     '''
@@ -64,29 +20,3 @@ def get_input():
         args = [args]
 
     return operation, args
-
-# Main game loop
-while True:
-    # Print enemy output
-    print_enemy_health(enemy)
-
-    # Get input
-    operation, args = get_input()
-
-    if operation == 'play':
-        # Attack with the card
-        attack(args[0], enemy)
-    elif operation == 'quit':
-        break
-    elif operation == '<3':
-        print('3<')
-    else:
-        print('unrecognized command')
-
-    # If the enemy is dead, then break out of the loop
-    if len(enemy) == 0:
-        break
-
-print('End of game!')
-if len(enemy) == 0:
-    print('You won!')
