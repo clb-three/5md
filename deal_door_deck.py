52d2354956699f4834bfb67e80e4b889822103fd
Ben Steenhoek <benjis@iastate.edu>
2020-04-06 14:25:11 -0500

    Draw picks random door card from list

--- deal_door_deck.py
+++ deal_door_deck.py
@@ -1,10 +1,41 @@
 
 from door_card import DoorCard
+import random
+
+door_cards = [
+    {
+        "name": "Slime",
+        "symbols": ["jump", "arrow"],
+        "type": "monster",
+    },
+    {
+        "name": "Sir Fuzzylumps",
+        "symbols": ["jump", "arrow", "arrow"],
+        "type": "monster",
+    }
+]
+
+
+def random_door_card():
+    return random.choice(door_cards)
+
+
+def draw_door_card():
+    '''
+    Draw a random door card from our big stash and initialize it.
+    '''
+
+    card = random_door_card()
+
+    return DoorCard(card["symbols"])
 
 
 def deal_door_deck(num_cards):
     '''
     Deal out and return a DoorDeck with the given num_cards.
     '''
+
+    # TODO: Add 2 challenge cards per player.
+
     symbols = ['sword', 'shield', 'arrow']
-    return [DoorCard(symbols) for _ in range(0, num_cards)]
+    return [draw_door_card() for _ in range(0, num_cards)]
