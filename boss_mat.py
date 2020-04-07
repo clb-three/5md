c8f6c7198e31645d623b93e59ec29157609bc81d
Ben Steenhoek <benjis@iastate.edu>
2020-04-07 00:48:18 -0500

    Refactor play_card logic into Table

--- boss_mat.py
+++ boss_mat.py
@@ -1,49 +1,11 @@
 
 
-import enum
 from attackable import Attackable
 
 
-class PlayResult(enum.Enum):
-    SuccessfulAttack = 1
-    FailedAttack = 2
-    DefeatedDoorDeck = 3
-    DefeatedEnemy = 4
-    DefeatedBoss = 5
-
-
 class Boss(Attackable):
-    pass
-
-
-class BossMat:
-    '''
-    A boss mat has a deck of door cards,
-    with a powerful boss on the bottom.
-    '''
-
-    def __init__(self, factory, num_cards, symbols):
-        self.door_deck = factory.create(num_cards)
-        self.target = self.door_deck.current_enemy
-
-        self.boss = Boss(symbols)
-
-    def is_defeated(self):
-        return self.target == self.boss and self.target.is_dead()
-
-    def play(self, symbol):
-        success = self.target.try_attack(symbol)
-
-        # switch to next enemy or boss when all enemies are dead
-        if self.target.is_dead():
-            if self.target == self.boss:
-                return PlayResult.DefeatedBoss
-
-            if self.door_deck.try_draw():
-                self.target = self.door_deck.current_enemy
-                return PlayResult.DefeatedEnemy
-
-            self.target = self.boss
-            return PlayResult.DefeatedDoorDeck
-
-        return PlayResult.SuccessfulAttack if success else PlayResult.FailedAttack
+    def __init__(self, symbols, num_door_cards):
+        # TODO: These should come from store somewhere,
+        # and be referenced just by the boss's name.
+        super().__init__(symbols)
+        self.num_door_cards = num_door_cards
--- game.py
+++ game.py
@@ -2,19 +2,26 @@
 
 from input import get_input
 from door_card import DoorCard
-from boss_mat import BossMat, PlayResult
+from boss_mat import Boss
 from door_deck_factory import DoorDeckFactory
 from hero import Hero
 import hero_cards
 
 
 class Table:
+    '''
+    A Table takes care of the logic for high-level interaction
+    between parts of the game.
+    '''
 
-    def __init__(self, heroes, mat):
+    def __init__(self, heroes, door_deck, boss):
         self.heroes = heroes
-        self.mat = mat
 
         self.game_over = False
+
+        self.door_deck = door_deck
+        self.boss = boss
+        self.target = self.door_deck.current_enemy
 
     def play_card(self, hero, card):
         '''
@@ -26,25 +33,35 @@ class Table:
             print(f"{hero.name} doesn't have that card!\n")
             return
 
-        result = self.mat.play(card)
-        if result == PlayResult.SuccessfulAttack:
-            print(f"{hero.name} hit the enemy with your {card}!\n")
-        elif result == PlayResult.FailedAttack:
-            print("That card has no effect on this enemy!")
-        elif result == PlayResult.DefeatedEnemy:
-            print(f'{hero.name} defeated a fearsome enemy. Grr!')
-        elif result == PlayResult.DefeatedDoorDeck:
-            print('Now we\'re fighting the boss. Ahh!')
-        elif result == PlayResult.DefeatedBoss:
-            print(f'{hero.name} killed the boss!')
-
         hero.discard(card)
+
+        success = self.target.try_attack(card)
+
+        # switch to next enemy or boss when all enemies are dead
+        if self.target.is_dead():
+            if self.target == self.boss:
+                print(f'{hero.name} killed the boss!')
+                return
+
+            if self.door_deck.try_draw():
+                print(f'{hero.name} defeated a fearsome enemy. Grr!')
+                self.target = self.door_deck.current_enemy
+                return
+            else:
+                print('Now we\'re fighting the boss. Ahh!')
+                self.target = self.boss
+                return
+
+        if success:
+            print(f"{hero.name} hit the enemy with your {card}!\n")
+        else:
+            print("That card has no effect on this enemy!")
 
     def play_turn(self):
         # Print enemy HP
         # TODO Issue #7: This will be removed and replaced with the print command.
         print(list(heroes.values()))
-        print(mat.target)
+        print(self.target)
 
         # Get input
         args = get_input()
@@ -86,13 +103,14 @@ class Table:
             print('Unrecognized command')
 
         # break out when all enemies isded
-        if mat.is_defeated():
+        if self.target == self.boss and self.boss.is_dead():
             print('You won!')
             self.game_over = True
 
 
+boss = Boss(['arrow', 'jump'], 20)
 door_deck_factory = DoorDeckFactory()
-mat = BossMat(door_deck_factory, 1, ['arrow', 'jump'])
+door_deck = door_deck_factory.create(boss.num_door_cards)
 
 # Each player will have one Hero.
 heroes = {
@@ -105,6 +123,6 @@ for j in range(0, 5):
     for h in heroes.values():
         h.draw_card()
 
-table = Table(heroes, mat)
+table = Table(heroes, door_deck, boss)
 while not table.game_over:
     table.play_turn()
