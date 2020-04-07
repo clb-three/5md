785c1b7ba371b169841183849e9bddabc5ce7f67
Ben Steenhoek <benjis@iastate.edu>
2020-04-07 00:19:50 -0500

    Functionize main loop to reduce global scope

--- game.py
+++ game.py
@@ -7,22 +7,8 @@ from door_deck_factory import DoorDeckFactory
 from hero import Hero
 import hero_cards
 
-door_deck_factory = DoorDeckFactory()
-mat = BossMat(door_deck_factory, 1, ['arrow', 'jump'])
 
-# Each player will have one Hero.
-heroes = {
-    "benji": Hero(),
-    "austin": Hero(),
-}
-
-# Draws the hero's initial hand
-for j in range(0, 5):
-    for h in heroes.values():
-        h.draw_card()
-
-
-def play_card(hero, card):
+def play_card(hero, card, mat):
     '''
     Play a card against the current enemy
     '''
@@ -55,48 +41,61 @@ def play_card(hero, card):
     hero.discard(card)
 
 
-# Main game loop
-while True:
+def main_loop(heroes, mat):
+    while True:
+        # Print enemy HP
+        # TODO Issue #7: This will be removed and replaced with the print command.
+        print(heroes)
+        print(mat.target)
 
-    # Print enemy HP
-    # TODO Issue #7: This will be removed and replaced with the print command.
-    print(heroes)
-    print(mat.target)
+        # Get input
+        args = get_input()
 
-    # Get input
-    args = get_input()
+        # Do input
+        if args[0] in heroes:
+            hero = heroes[args[0]]
 
-    # Decide what operation to do
-    if args[0] in heroes:
-        hero = heroes[args[0]]
+            if args[1] == 'play':
+                play_card(hero, args[2], mat)
+            elif args[1] == 'discard':
+                hero.discard(args[2])
+            elif args[1] == 'draw':
+                card_drawn = hero.draw_card()
+                print('You drew a %s.' % card_drawn)
+                print('Your deck has %d cards left.' % len(hero.hero_deck))
+        elif args[0] == 'print':
+            # TODO Issue #7: Print the current enemy's deets. We'll want this to replace the printing
+            # the status up in the top of the loop.
+            print('Not implemented yet')
+        elif args[0] == 'quit':
+            # Quit the game
+            break
+        elif args[0] == '<3':
+            # Love on u
+            print('3<')
+        else:
+            # Catch any command that we don't know
+            # and let the user know about it
+            print('Unrecognized command')
 
-        if args[1] == 'play':
-            play_card(hero, args[2])
-        elif args[1] == 'discard':
-            hero.discard(args[2])
-        elif args[1] == 'draw':
-            card_drawn = hero.draw_card()
-            print('You drew a %s.' % card_drawn)
-            print('Your deck has %d cards left.' % len(hero.hero_deck))
-    elif args[0] == 'print':
-        # TODO Issue #7: Print the current enemy's deets. We'll want this to replace the printing
-        # the status up in the top of the loop.
-        print('Not implemented yet')
-    elif args[0] == 'quit':
-        # Quit the game
-        break
-    elif args[0] == '<3':
-        # Love on u
-        print('3<')
-    else:
-        # Catch any command that we don't know
-        # and let the user know about it
-        print('Unrecognized command')
+        # break out when all enemies isded
+        if mat.is_defeated():
+            print('You won!')
+            break
 
-    # break out when all enemies isded
-    if mat.is_defeated():
-        break
 
-print('End of game!')
-if mat.is_defeated():
-    print('You won!')
+door_deck_factory = DoorDeckFactory()
+mat = BossMat(door_deck_factory, 1, ['arrow', 'jump'])
+
+# Each player will have one Hero.
+heroes = {
+    "benji": Hero(),
+    "austin": Hero(),
+}
+
+# Draws the hero's initial hand
+for j in range(0, 5):
+    for h in heroes.values():
+        h.draw_card()
+
+main_loop(heroes, mat)
