8665334e468236461dbe5f9facbd03b0adfcb932
bstee615 <benjaminjsteenhoek@gmail.com>
2020-06-30 23:08:58 -0500

    Add webpack plugins for HTML

--- ui/src/input.js
+++ ui/src/input.js
@@ -2,10 +2,16 @@ let socket;
 
 export function initialize(sock) {
   socket = sock;
-  const input = document.getElementById("command");
-  const submit = document.getElementById("submit-command");
+
+  const input = document.createElement("input");
+  input.placeholder = "Enter command here";
+  document.body.appendChild(input);
+
+  const submit = document.createElement("button");
+  submit.innerHTML = "Send";
   submit.addEventListener("click", function () {
     socket.emit("command", input.value);
     input.value = "";
   });
+  document.body.appendChild(submit);
 }
--- ui/webpack.config.js
+++ ui/webpack.config.js
@@ -1,20 +1,31 @@
+const path = require("path");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
-  devtool: "source-map",
+  devtool: "inline-source-map",
+  mode: "development",
   output: {
-    filename: "main.js",
+    filename: "[name].[contenthash].js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000,
   },
   plugins: [
     new CopyWebpackPlugin({
       patterns: [
         {
-          from: "./*.html",
-        },
-        {
           from: "./images/*",
         },
       ],
+    }),
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: "Output Management",
     }),
   ],
 };
