f176e390afa2085ad90c536b8c18b6484d84bd08
bstee615 <benjaminjsteenhoek@gmail.com>
2020-09-11 11:23:27 -0500

    Remove log

--- ui/src/log.js
+++
@@ -1,9 +0,0 @@
-function now() {
-  return new Date().toLocaleString("en-US");
-}
-
-function initialized(module) {
-  console.log(`${now()} initialized ${module}`);
-}
-
-export default { initialized };
