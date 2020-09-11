7bcdc4cf81b52d0f04c2e6f10b51b59e77c087ac
bstee615 <benjaminjsteenhoek@gmail.com>
2020-09-11 14:43:43 -0500

    Move debugging fixture to debug.js

--- ui/src/index.js
+++ ui/src/index.js
@@ -1,22 +1,8 @@
 // debug
 import {Model} from "./Model";
-import {initializeSocket, socket} from "./socket";
-
-// display
-
-function initializeDebugElements() {
-    const input = document.createElement("input");
-    input.placeholder = "Enter command here";
-    document.body.appendChild(input);
-
-    const submit = document.createElement("button");
-    submit.innerHTML = "Send";
-    submit.addEventListener("click", function () {
-        socket.emit("command", input.value);
-        input.value = "";
-    });
-    document.body.appendChild(submit);
-}
+import {initializeSocket} from "./socket";
+import {initializeDebugElements} from "./debug";
+import {Display} from "./Display";
 
 initializeDebugElements();
 
