4b1661c3fbdedfa8d879a6c93648dcc0b67ad036
bstee615 <benjaminjsteenhoek@gmail.com>
2020-10-07 17:37:44 -0500

    Add discard_n and discard_all scripts

--- model/doorcards/scripts.py
+++ model/doorcards/scripts.py
@@ -9,5 +9,17 @@ def all_heroes_discard_one(ctx):
             hero.discard(choice)
 
 
+def all_heroes_discard_n(ctx, n):
+    """Make all heroes discard n cards"""
+    for _ in range(n):
+        all_heroes_discard_one(ctx)
+
+
+def all_heroes_discard_all(ctx):
+    """Make all heroes discard all cards"""
+    while any(len(hero.hand) > 0 for hero in ctx.heroes):
+        all_heroes_discard_one(ctx)
+
+
 script_funcs = [all_heroes_discard_one]
 scripts = {f.__name__: f for f in script_funcs}
