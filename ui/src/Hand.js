52fdc1ea2810c10a5c19e9543dd14d5c0bda7d9f
bstee615 <benjaminjsteenhoek@gmail.com>
2020-10-20 18:49:26 -0500

    Refactor card logic into card class

--- ui/src/Hand.js
+++ ui/src/Hand.js
@@ -3,42 +3,65 @@ import * as loglevel from "loglevel";
 
 const log = loglevel.getLogger("display::Hand");
 
-function onDragStart(ev, cardDisplay) {
-    log.info("onDragStart");
-    cardDisplay.data = ev.data;
-    cardDisplay.dragging = true;
-}
+class Card {
+    constructor(display, uuid, target) {
+        this.uuid = uuid;
+        this.display = display;
+        this.target = target;
 
-function onDragEnd(ev, cardDisplay, name, deck) {
-    log.info("onDragEnd");
-    cardDisplay.dragging = false;
-    cardDisplay.data = null;
-
-    const inGarbage = false;
-    const shouldPlay = deck.containsPoint(ev.data.global);
-    if (inGarbage) {
-        socket.emit("command", `hero benji discard ${name}`);
-    } else if (shouldPlay) {
-        socket.emit("command", `hero benji play ${name}`);
+        this.display.interactive = true;
+        this.display.buttonMode = true;
+        this.display
+            // events for drag start
+            .on('mousedown', ev => this.onDragStart(ev))
+            .on('touchstart', ev => this.onDragStart(ev))
+            // events for drag end
+            .on('mouseup', ev => this.onDragEnd(ev, this.deck))
+            .on('mouseupoutside', ev => this.onDragEnd(ev, this.deck))
+            .on('touchend', ev => this.onDragEnd(ev, this.deck))
+            .on('touchendoutside', ev => this.onDragEnd(ev, this.deck))
+            // events for drag move
+            .on('mousemove', ev => this.onDragMove())
+            .on('touchmove', ev => this.onDragMove());
     }
-}
 
-function onDragMove(cardDisplay) {
-    if (cardDisplay.dragging) {
-        log.debug("onDragMove");
-        const newPosition = cardDisplay.data.getLocalPosition(cardDisplay.parent);
-        cardDisplay.position.x = newPosition.x;
-        cardDisplay.position.y = newPosition.y;
+    onDragStart(ev) {
+        log.info("onDragStart");
+        this.display.data = ev.data;
+        this.display.dragging = true;
+    }
+
+    onDragEnd(ev, deck) {
+        log.info("onDragEnd");
+        this.display.dragging = false;
+        this.display.data = null;
+
+        const inGarbage = false;
+        const shouldPlay = this.target.containsPoint(ev.data.global);
+        if (inGarbage) {
+            socket.emit("command", `hero benji discard ${this.uuid}`);
+        } else if (shouldPlay) {
+            socket.emit("command", `hero benji play ${this.uuid}`);
+        }
+    }
+
+    onDragMove() {
+        if (this.display.dragging) {
+            log.debug("onDragMove");
+            const newPosition = this.display.data.getLocalPosition(this.display.parent);
+            this.display.position.x = newPosition.x;
+            this.display.position.y = newPosition.y;
+        }
     }
 }
 
 export class Hand {
-    constructor(display, deck) {
+    constructor(display, target) {
         this.handX = 100;
         this.cardDisplay = {};
         this.display = display;
         this.dragging = false;
-        this.deck = deck;
+        this.target = target;
 
         log.debug("hand x", this.handX);
     }
@@ -46,28 +69,9 @@ export class Hand {
     drawCard(card) {
         log.debug("draw", card);
 
-        const name = card.symbol;
         const y = 100;
-
-        // load the texture we need
-        const cardDisplay = this.display.sprite(`images/${name}.png`, this.handX, y, 100, 160);
-
-        cardDisplay.interactive = true;
-        cardDisplay.buttonMode = true;
-        cardDisplay
-            // events for drag start
-            .on('mousedown', ev => onDragStart(ev, cardDisplay))
-            .on('touchstart', ev => onDragStart(ev, cardDisplay))
-            // events for drag end
-            .on('mouseup', ev => onDragEnd(ev, cardDisplay, name, this.deck))
-            .on('mouseupoutside', ev => onDragEnd(ev, cardDisplay, name, this.deck))
-            .on('touchend', ev => onDragEnd(ev, cardDisplay, name, this.deck))
-            .on('touchendoutside', ev => onDragEnd(ev, cardDisplay, name, this.deck))
-            // events for drag move
-            .on('mousemove', ev => onDragMove(cardDisplay))
-            .on('touchmove', ev => onDragMove(cardDisplay));
-
-        this.cardDisplay[card.uuid] = cardDisplay;
+        const sprite = this.display.sprite(`images/${card.name}.png`, this.handX, y, 100, 160);
+        this.cardDisplay[card.uuid] = new Card(sprite, card.uuid, this.target);
         this.handX += 60;
     }
 
@@ -75,7 +79,7 @@ export class Hand {
         log.debug("discard", card);
 
         if (card) {
-            this.display.deleteThisNephew(this.cardDisplay[card.uuid]);
+            this.display.deleteThisNephew(this.cardDisplay[card.uuid].display);
             delete this.cardDisplay[card.uuid];
         }
     }
